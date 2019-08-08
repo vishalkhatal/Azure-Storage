197e9d7eadcdf26aaa77798510f85405d4df9229
Vishal Khatal <vishal.khatal2206@gmail.com>
2019-08-08 19:12:34 +0530

    Added other queue operations

--- AzureQueueStorage/AzureQueueStorage/Program.cs
+++ AzureQueueStorage/AzureQueueStorage/Program.cs
@@ -14,8 +14,7 @@ namespace AzureQueueStorage
         static void Main(string[] args)
         {
             // Retrieve storage account from connection string.
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-                ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
 
             // Create the queue client.
             CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
@@ -33,8 +32,25 @@ namespace AzureQueueStorage
             // Peek at the next message
             CloudQueueMessage peekedMessage = queue.PeekMessage();
 
+
             // Display message.
             Console.WriteLine(peekedMessage.AsString);
+
+            // Get the next message
+            CloudQueueMessage retrievedMessage = queue.GetMessage();
+
+            //Process the message in less than 30 seconds, and then delete the message
+            queue.DeleteMessage(retrievedMessage);
+
+            // Peek at the next message
+            CloudQueueMessage peekedMessageAgain = queue.PeekMessage();
+
+            foreach (CloudQueueMessage newMessage in queue.GetMessages(20, TimeSpan.FromMinutes(5)))
+            {
+                // Process all messages in less than 5 minutes, deleting each message after processing.
+                queue.DeleteMessage(newMessage);
+            }
+
             Console.ReadKey();
         }
     }
